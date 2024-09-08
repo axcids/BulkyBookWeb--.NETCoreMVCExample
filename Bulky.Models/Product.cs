7a045897af23a7b72b67db64d6efeef502095328
abilal <abilal@moe.gov.sa>
2024-09-08 07:36:35 +0300

    update

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace BulkyBook.Models {
     public class Product {
@@ -46,6 +47,7 @@ namespace BulkyBook.Models {
         public double Price100 { get; set; }
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
+        [ValidateNever]
         public Category Category { get; set; }
         public string ImageUrl { get; set; }
     }
--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -23,55 +23,76 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             //I need to learn this more
             return View(objProductList);
         }
-        public IActionResult Create()
+        //--------------------------------------------------------------------------------------------------------------------------------Create <- Upsert Now 
+        public IActionResult Upsert(int? id)
         {
-            ProductVM productVM = new() {
-                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem { Text = u.Name, Value = u.Id.ToString() }),
+            ProductVM productVM = new () {
+                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem { 
+                    Text = u.Name, 
+                    Value = u.Id.ToString() 
+                }),
                 Product = new Product()
             };
-            return View(productVM);
+            if(id == null || id == 0) {
+                //It means it is a create
+                return View(productVM);
+
+            }
+            else {
+                //It means it is an update 
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                return View(productVM);
+            }
         }
         [HttpPost]
-        public IActionResult Create(ProductVM obj)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
 
 
             if (ModelState.IsValid)
             {
                 TempData["Success"] = "Product created successfully!";
-                _unitOfWork.Product.Add(obj.Product);
+                _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 return RedirectToAction("Index");
             }
-            return View();
+            else {
+                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+                return View(productVM);
+            }
 
         }
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
+        //---------------------------------------------------------------------------------------------------------------------------------Edit (removed and replaced with upsert)
+        //public IActionResult Edit(int? id)
+        //{
+        //    if (id == null || id == 0)
+        //    {
+        //        return NotFound();
+        //    }
+        //    Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+        //    if (productFromDb == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(productFromDb);
+        //}
+        //[HttpPost]
+        //public IActionResult Edit(Product obj)
+        //{
 
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                return RedirectToAction("Index");
-            }
-            return View();
+        //    if (ModelState.IsValid)
+        //    {
+        //        _unitOfWork.Product.Update(obj);
+        //        _unitOfWork.Save();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View();
 
-        }
+        //}
+        //-------------------------------------------------------------------------------------------------------------------------------Delete
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
