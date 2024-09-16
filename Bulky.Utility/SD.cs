d0cd5f04b29d505e28ff31190e2cd20fa8e2bdcb
axcids <axcids@gmail.com>
2024-09-16 15:37:57 +0300

    update

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.Utility {
     public static class SD {
-
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
     }
 }
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -2,6 +2,9 @@ using BulkyBook.DataAccess.Data;
 using BulkyBook.DataAccess.Repository;
 using BulkyBook.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace BulkyBookWeb {
     public class Program {
@@ -11,7 +14,11 @@ namespace BulkyBookWeb {
             // Add services to the container.
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddRazorPages();
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddScoped<IEmailSender, EmailSender>();
+
 
             var app = builder.Build();
 
@@ -26,9 +33,9 @@ namespace BulkyBookWeb {
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
-
+            app.MapRazorPages();
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
