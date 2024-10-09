97f28378f4cbc027dfacc68701c4de40cd1f2809
axcids <axcids@gmail.com>
2024-10-09 11:30:47 +0300

    update

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -67,6 +67,24 @@ namespace BulkyBookWeb.Areas.Admin.Controllers {
             TempData["Success"] = "Order details updated successfully!";
             return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
         }
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult ShipOrder() {
+
+            var orderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == OrderVM.OrderHeader.Id);
+            orderHeader.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+            orderHeader.Carrier = OrderVM.OrderHeader.Carrier;
+            orderHeader.OrderStatus = SD.StatusShipped;
+            orderHeader.ShippingDate = DateTime.Now;
+            if(orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment) {
+                orderHeader.PaymentDueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(30));
+            }
+
+            _unitOfWork.OrderHeader.Update(orderHeader);
+            _unitOfWork.Save();
+            TempData["Success"] = "Order details updated successfully!";
+            return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
+        }
 
 
         #region API CALLS
